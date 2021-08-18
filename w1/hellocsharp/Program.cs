2ad009fa238d102e8cae068d2824e3b19e0161cd
alex duff <alexander.duff@revature.net>
2021-08-17 20:50:45 -0400

    added calculator pseudocode

--- w1/hellocsharp/Program.cs
+++ w1/hellocsharp/Program.cs
@@ -10,3 +10,23 @@ namespace hellocsharp
         }
     }
 }
+//Calculator pseudocode>
+/* 
+int x
+int y
+int answer
+char calc
+Scanner scanner = new Scanner
+  system.out.println ("enter numbers)
+  x =scanner.nextint
+  y = scanner.nextint
+system.out.println("what kind of calc")
+calc =scanner.nextchar
+switch (calc)
+  case + : answer=x+y
+  case - : answer=x-y
+  case / : answer=x/y
+  case * : answer=x*y
+  default: system.out.println("enter +,-,*,/)
+  system.out.println(answer)
+   */
